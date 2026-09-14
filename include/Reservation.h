108e7bbdb935b472e163bfa88903af19324f7208
fmucheke <frankmucheke399@gmail.com>
2026-09-14 14:44:27 -0500

    Made the foundation for Reservation and ReservationManager header files

--- include/Reservation.h
+++ include/Reservation.h
@@ -1,1 +1,29 @@
+#ifndef RESERVATION_H
+#define RESERVATION_H
+#include <string>
+using namespace std;
 
+class Reservation{
+private:
+    int ReservationID;
+    int StudentID;
+    string StudentName;
+    string ResourceID;
+    string ReservationDate;
+
+public:
+    Reservation(int Reservation_ID, int Student_ID, string Student_Name, string Resource_ID, string Reservation_Date);
+
+    int GetReservation_ID() const;
+    int GetStudent_ID() const;
+    string GetStudent_Name() const;
+    string GetResource_ID() const;
+    string GetReservation_Date() const;
+    void display() const;
+
+
+
+
+};
+
+#endif
--- include/ReservationManager.h
+++ include/ReservationManager.h
@@ -1,1 +1,36 @@
+#ifndef RESERVATIONMANAGER_H
+#define RESERVATIONMANAGER_H
+#include <string>
+#include "Reservation.h"
 
+class ReservationManager{
+private:
+    struct Node
+    {
+        Reservation reservation;
+        Node* next;
+
+        Node();
+    };
+
+    Node* head;
+
+public:
+
+    ReservationManager();
+
+    ~ReservationManager();
+
+    void InsertReservation(Reservation);
+
+    bool RemoveReservation(int ReservationID);
+
+    void DisplayReservations() const;
+
+    bool ReservationExists(int ReservationID) const;
+
+    bool ValidateReservation(int ReservationID, string StudentName, int StudentID, string ResourceID, string ReservationDate) const;
+
+};
+
+#endif
