ca55f3e9b2e8e5fad862cd561acef78a4222ccb4
S-UPRETI <samriddhiupreti1@gmail.com>
2026-09-16 23:37:49 -0500

    Implement CancellationHistory class with stack functionality
    Added CancellationHistory class with methods for managing cancellation history.

--- include/CancellationHistory.h
+++ include/CancellationHistory.h
@@ -1,1 +1,32 @@
 //CancellationHistory header placeholder- samie
+#ifndef CANCELLATIONHISTORY_H
+#define CANCELLATIONHISTORY_H
+#include "Reservation.h" //pulls reservation class definatoon as our calss needs to know reservation  is to store
+
+class CancellationHistory {
+private:
+	struct Node {
+		Reservation data;
+		Node* next;
+
+		Node(Reservation r) : data(r), next(nullptr) {} // This is the Node's constructor: when you create a Node and pass it a Reservation (r),
+        // it automatically stores that reservation in "data" and sets "next" to nullptr
+
+	};
+	Node* top;// Pointer to the Node currently on top of the stack (the most recently cancelled reservation)
+	int count;
+
+public:
+	CancellationHistory();
+	~CancellationHistory(); //Destructor declaration
+
+	void pushCancellation(Reservation r);
+
+	Reservation popAndRestore();
+
+	bool isEmpty() const;
+	int getCount() const;
+	void displayHistory() const;
+
+};
+#endif//CancellationHistory header placeholder- samie
