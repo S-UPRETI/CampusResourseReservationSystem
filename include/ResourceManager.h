d9d25423a914791d8bd1ed3eac37e36638376f3f
urishashrestha <urishashrestha@my.unt.edu>
2026-09-18 12:01:04 -0500

    Add ResourceManager class declaration

--- include/ResourceManager.h
+++ include/ResourceManager.h
@@ -1,1 +1,37 @@
+#ifndef RESOURCEMANAGER_H
+#define RESOURCEMANAGER_H
 
+#include <string>
+#include <vector>
+#include "Resource.h"
+
+
+class ResourceManager {
+private:
+    std::vector<Resource> resources;
+
+public:
+    ResourceManager();
+
+    bool loadFromFile(const std::string& filename);
+
+   
+    void displayAll() const;
+
+   
+    void displayAvailable() const;
+
+   
+    Resource* findResource(const std::string& resourceID);
+
+   
+    bool setResourceAvailability(const std::string& resourceID, bool available);
+
+   
+    int getResourceCount() const;
+
+    
+    const std::vector<Resource>& getAllResources() const;
+};
+
+#endif 
